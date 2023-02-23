d3c4bf3c733c3c6d08dc4a0521d7b733b3c466c8
alebarre.Alexandre Silva <alebarre.plotartemail@gmail.com>
2023-02-23 11:46:44 -0300

    Aula 7.4 deploy Hheroku

--- src/main/java/com/example/algamoney/api/AlgamoneyApiApplication.java
+++ src/main/java/com/example/algamoney/api/AlgamoneyApiApplication.java
@@ -2,6 +2,9 @@ package com.example.algamoney.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.example.algamoney.api.config.property.AlgamoneyApiProperty;
 
 @SpringBootApplication
 public class AlgamoneyApiApplication {
--- src/main/java/com/example/algamoney/api/repository/LancamentoRepository.java
+++ src/main/java/com/example/algamoney/api/repository/LancamentoRepository.java
@@ -7,6 +7,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LancamentoRepository extends JpaRepository<Lancamento, Long>, LancamentoRepositoryQuery {
-    
-}
+public interface LancamentoRepository extends JpaRepository<Lancamento, Long>, LancamentoRepositoryQuery {}
